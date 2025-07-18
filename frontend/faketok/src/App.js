98d27a997f51a6f7da50b9f9457484a6d75ee1d7
sujpatel <sujaypat3@gmail.com>
2025-07-18 18:48:22 -0400

    created input box to paste url, called backend

--- frontend/faketok/src/App.js
+++ frontend/faketok/src/App.js
@@ -1,10 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
 
+  const [url, setUrl] = useState("");
+  const [data, setData] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:8000/analyze", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          url: url
+        }),
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP Error, status: ${response.status}`);
+      }
+      const result = await response.json();
+      setData(result);
+      setUrl("");
+    } catch (error) {
+      console.error("Error during fetch:", error);
+    }
+
+
+
+  }
+
   return (
     <div>
-      <input></input>
+      <form onSubmit={handleSubmit}>
+        <input value={url} onChange={(e) => setUrl(e.target.value)}></input>
+        <button type="submit">Analyze</button>
+      </form>
 
     </div >
   );
