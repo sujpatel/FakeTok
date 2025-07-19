49d9a3a9d5279a99bb6b846e48b61d3610b0f019
sujpatel <sujaypat3@gmail.com>
2025-07-19 12:51:34 -0400

    set up yt-dlp to download tiktok, ffmpeg to extract audio from tiktok

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from yt_dlp import YoutubeDL
+import os
+import subprocess
 
 app = FastAPI()
 
@@ -17,12 +20,44 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def download_tiktok_video(url):
+    os.makedirs('downloads', exist_ok=True)
+    ydl_opts = {
+        'outtmpl': 'downloads/%(id)s.%(ext)s',
+        'format' : 'bestvideo+bestaudio/best',
+        'merge_output_format': 'mp4',
+    }
+    with YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        file_path = ydl.prepare_filename(info)
+        return info['id'], file_path
+
+def extract_audio(video_path, output_audio_path):
+    command = [
+        "ffmpeg",
+        "-i", video_path,
+        "-vn",
+        "-acodec", "mp3",
+        output_audio_path
+    ]
+    subprocess.run(command, check=True)
+
 class UrlInput(BaseModel):
     url: str
 
 @app.post("/analyze")
 async def analyze_url(data: UrlInput):
-    return {"status": "success", "received_url": data.url}
+    video_id, path = download_tiktok_video(data.url)
+    
+    audio_path = f"downloads/{video_id}.mp3"
+    extract_audio(path, audio_path)
+    
+    return {
+        "status": "success", 
+        "video_id": video_id,
+        "file_path": path,
+        "audio_file": audio_path
+        }
 
 
 
