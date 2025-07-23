c4c899797e46eaa1d396184e3b204c50f7ff0ac2
sujpatel <sujaypat3@gmail.com>
2025-07-23 16:05:24 -0400

    set up tailwindcss / added styling to ui

--- frontend/faketok/src/App.js
+++ frontend/faketok/src/App.js
@@ -35,10 +35,11 @@ function App() {
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input value={url} onChange={(e) => setUrl(e.target.value)}></input>
-        <button type="submit">Analyze</button>
+    <div className="min-h-screen flex items-center justify-center text-white">
+      <form onSubmit={handleSubmit} className="flex flex-col items-center gap-4 mt-16 px-4">
+        <input type="text"
+          placeholder="Paste TikTok URL here!" value={url} onChange={(e) => setUrl(e.target.value)} className="w-[90vw] max-w-2xl px-4 py-3 mb-10 bg-[#010101] text-white  border-[#69C9D0] border-[2px] placeholder-[#EE1D52] rounded-xl focus:outline-none focus:ring-4 focus:ring-[#69C9D0]   transition"></input>
+        <button type="submit" className="px-6 py-3 font-semibold text-white bg-[#EE1D52] hover:bg-[#d81a4b] rounded-xl transition">Analyze</button>
       </form>
 
     </div >
